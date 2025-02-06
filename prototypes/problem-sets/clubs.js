eb8b8835990f99b6474ed5ac70656bfcadd54280
Jaren Garman <jarengarman@protonmail.com>
2025-02-05 19:12:25 -0500

    chore: Fix formatting

--- prototypes/problem-sets/clubs.js
+++ prototypes/problem-sets/clubs.js
@@ -9,14 +9,14 @@ console.log('Running clubs.js')
 /*
 Level 1
 
-Code: 
-  Write a function called "findClubMembers" that takes in the clubs data as an argument.  Your function should create an object whose keys are the names of people, and whose values are arrays that include the names of the clubs that person is a part of. 
+Code:
+  Write a function called "findClubMembers" that takes in the clubs data as an argument.  Your function should create an object whose keys are the names of people, and whose values are arrays that include the names of the clubs that person is a part of.
 
-Invoke: 
+Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(findClubMembers(clubs))
-    should print --> 
+    should print -->
     {
       Louisa: ['Drama', 'Art'],
       Pam: ['Drama', 'Art', 'Chess'],
@@ -25,7 +25,7 @@ e.g.
     }
 
 Annotate:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -40,8 +40,8 @@ Test:
   *Refactor as needed until the test is passing
 
 Annotation:
-  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions. 
-  Take notes on the error messages that led you to fixing those details. 
+  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions.
+  Take notes on the error messages that led you to fixing those details.
 */
 
 
