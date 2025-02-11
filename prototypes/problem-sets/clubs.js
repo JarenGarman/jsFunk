17451b16842a072b25be288aafa787caf660e53f
Jaren Garman <jarengarman@protonmail.com>
2025-02-11 15:28:51 -0500

    feat: Complete clubs

--- prototypes/problem-sets/clubs.js
+++ prototypes/problem-sets/clubs.js
@@ -70,8 +70,6 @@ Annotation:
   Take notes on the error messages that led you to fixing those details.
 */
 
-
-
-// module.exports = {
-//   findClubMembers
-// };
+module.exports = {
+  findClubMembers
+};
