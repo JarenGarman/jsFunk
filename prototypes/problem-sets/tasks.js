bed255f386ef85dd0e957fa1afb419dcab47f7fa
Jaren Garman <jarengarman@protonmail.com>
2025-02-11 16:57:27 -0500

    chore: Formatting

--- prototypes/problem-sets/tasks.js
+++ prototypes/problem-sets/tasks.js
@@ -1,4 +1,4 @@
-const { tasks } = require('../datasets/tasks');
+const { tasks } = require('../datasets/tasks')
 
 // To run the code you've written in this file, use node prototypes/problem-sets/tasks.js
 
@@ -9,25 +9,25 @@ console.log('Running tasks.js')
 /*
 Level 1
 
-Code: 
+Code:
   Write a function called "getAverageTime" that determines the average time for all tasks.
 
-Invoke: 
+Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getAverageTime())
-    should print -->      
+    should print -->
       'The average time for all tasks is 100 minutes.'
 
 Annotate:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
 /*
 Level 2
 
-Code: 
+Code:
   Write a function called "getTasksByPerson" that takes in an argument of a person's name and outputs a list of their assigned tasks.
 
 Invoke:
@@ -41,52 +41,52 @@ e.g.
     should print -->  [ 'debug issue #14', 'add feature #38' ]
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
 /*
 Level 3
 
-Code: 
+Code:
   Write a function called "getStatusTimes" that returns an object with the total minutes for each status.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getStatusTimes())
-    should print --> 
-    { 
-      inProgress: 180, 
-      complete: 350, 
-      inTriage: 170 
+    should print -->
+    {
+      inProgress: 180,
+      complete: 350,
+      inTriage: 170
     }
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
 /*
 Level 4
 
-Code: 
+Code:
   Refactor your "getAverageTime" function so that it takes in a status and returns the average time for tasks with that status.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getAverageTime("complete"))
-    should print -->      
+    should print -->
       'The average time for all complete tasks is 87.5 minutes.'
 
 e.g.
   console.log(getAverageTime("inProgress"))
-    should print -->      
+    should print -->
       'The average time for all inProgress tasks is 90 minutes.'
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -100,8 +100,8 @@ Test:
   * Refactor as needed until all tasks tests are passing
 
 Annotation:
-  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions. 
-  Take notes on the error messages that led you to fixing those details. 
+  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions.
+  Take notes on the error messages that led you to fixing those details.
 */
 
 
@@ -109,4 +109,4 @@ Annotation:
 //   getAverageTime,
 //   getTasksByPerson,
 //   getStatusTimes
-// };
+// }
