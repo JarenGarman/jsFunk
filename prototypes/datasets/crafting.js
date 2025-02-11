d0f1203720dfec4136682a34991d1b0748f983cb
Jaren Garman <jarengarman@protonmail.com>
2025-02-11 17:45:17 -0500

    chore: Formatting

--- prototypes/datasets/crafting.js
+++ prototypes/datasets/crafting.js
@@ -1,25 +1,25 @@
 const craftSupplies = {
   crossStitching: [
-    { name: 'fabric', amountNeeded: 0.25 }, 
-    { name: 'needle', amountNeeded: 1 }, 
-    { name: 'thread', amountNeeded: 10 }, 
+    { name: 'fabric', amountNeeded: 0.25 },
+    { name: 'needle', amountNeeded: 1 },
+    { name: 'thread', amountNeeded: 10 },
     { name: 'scissor', amountNeeded: 1 },
     { name: 'hoop', amountNeeded: 1 }
   ],
   weaving: [
-    { name: 'loom', amountNeeded: 1 }, 
-    { name: 'needle', amountNeeded: 1 }, 
-    { name: 'yarn', amountNeeded: 6 }, 
+    { name: 'loom', amountNeeded: 1 },
+    { name: 'needle', amountNeeded: 1 },
+    { name: 'yarn', amountNeeded: 6 },
     { name: 'scissor', amountNeeded: 1 }
   ],
-  knitting: [    
-    { name: 'needle', amountNeeded: 2 }, 
-    { name: 'yarn', amountNeeded: 4 }, 
+  knitting: [
+    { name: 'needle', amountNeeded: 2 },
+    { name: 'yarn', amountNeeded: 4 },
     { name: 'scissor', amountNeeded: 1 }
   ],
   crocheting: [
-    { name: 'hook', amountNeeded: 1 }, 
-    { name: 'yarn', amountNeeded: 3 }, 
+    { name: 'hook', amountNeeded: 1 },
+    { name: 'yarn', amountNeeded: 3 },
     { name: 'scissor', amountNeeded: 1 }
   ]
 }
