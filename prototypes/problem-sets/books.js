ca757cf3ad25769d2f53511be6b6f512d373dbb6
Jaren Garman <jarengarman@protonmail.com>
2025-02-12 17:34:20 -0500

    chore: Formatting

--- prototypes/problem-sets/books.js
+++ prototypes/problem-sets/books.js
@@ -1,4 +1,4 @@
-const { books } = require('../datasets/books');
+const { books } = require('../datasets/books')
 
 // To run the code you've written in this file, use node prototypes/problem-sets/books.js
 
@@ -9,20 +9,20 @@ console.log('Running books.js')
 /*
 Level 1
 
-Code: 
+Code:
   Write a function called "removeViolence" that takes in the books data as an argument and returns an array of all book titles that are not horror or true crime.
 
-Invoke: 
+Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(removeViolence(books))
-    should print --> 
+    should print -->
      [
         '1984', 'The Great Gatsby', 'Lord of the Flies', 'Harry Potter and the Sorcerer\'s Stone', 'The Hitchhiker\'s Guide to the Galaxy', 'Flowers for Algernon', 'Slaughterhouse-Five', 'The Handmaid\'s Tale', 'The Metamorphosis', 'Brave New World', 'Life of Pi', 'The Curious Incident of the Dog in the Night - Time', 'The Bell Jar', 'Catch-22', 'Treasure Island'
       ]
 
 Annotate:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -30,14 +30,14 @@ Annotate:
 /*
 Level 2
 
-Code: 
+Code:
   Write a function called "getByGenre" that takes 2 arguments: the books data, and a specified genre.  The function should return an array of objects containing all books of that genre.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getByGenre(books, "Fiction"))
-    should print -->   
+    should print -->
     [
       {
         title: 'The Great Gatsby',
@@ -62,7 +62,7 @@ e.g.
 
 e.g.
   console.log(getByGenre(books, "Satire"))
-    should print -->   
+    should print -->
     [
       {
         title: 'Catch-22',
@@ -73,7 +73,7 @@ e.g.
     ]
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -82,14 +82,14 @@ Annotation:
 /*
 Level 3
 
-Code: 
+Code:
   Modify your "getByGenre" function so that it doesn't return the entire book object, only the title.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getByGenre(books, "Fiction"))
-    should print -->   
+    should print -->
     [
       'The Great Gatsby',
       'Lord of the Flies',
@@ -100,11 +100,11 @@ e.g.
 
 e.g.
   console.log(getByGenre(books, "Satire"))
-    should print -->   
+    should print -->
     ['Catch-22']
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible.  
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -112,43 +112,43 @@ Annotation:
 /*
 Level 4
 
-Code: 
+Code:
   Modify your "getByGenre" function so that it takes in a third argument of a year and only returns the book titles for books published after that year.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getByGenre(books, "Fiction", 1954))
-    should print -->   
+    should print -->
     [ 'Life of Pi', 'The Bell Jar' ]
 
 e.g.
   console.log(getByGenre(books, "Science Fiction", 1950))
-    should print -->   
+    should print -->
     [
       "The Hitchhiker's Guide to the Galaxy",
       'Flowers for Algernon',
       'Slaughterhouse-Five',
       "The Handmaid's Tale"
     ]
-    
+
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible.  
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
-  
+
 
 
 /*
 Level 5
 
-Code: 
+Code:
   Write a function called "getNewBooks" that returns an array of objects containing all books that were published in the 90's and 00's. Each object should have only a title property and a year property.
 
 Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getNewBooks())
-    should print -->   
+    should print -->
     [
       { title: 'Harry Potter and the Sorcerer\'s Stone', year: 1997 },
       { title: 'Life of Pi', year: 2001 },
@@ -156,7 +156,7 @@ e.g.
     ]
 
 Annotation:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible.  
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -171,14 +171,12 @@ Test:
   * Refactor as needed until all Books tests are passing
 
 Annotation:
-  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions. 
-  Take notes on the error messages that led you to fixing those details. 
+  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions.
+  Take notes on the error messages that led you to fixing those details.
 */
-
-
 
 // module.exports = {
 //   removeViolence,
 //   getByGenre,
 //   getNewBooks,
-// };
+// }
