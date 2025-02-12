927725244b6656f098bbe79e24eaade4f4809c2f
Jaren Garman <jarengarman@protonmail.com>
2025-02-11 20:19:51 -0500

    chore: Formatting

--- prototypes/datasets/mods.js
+++ prototypes/datasets/mods.js
@@ -3,8 +3,8 @@ const mods = [
   { mod: 2, students: 33, instructors: 3 },
   { mod: 3, students: 20, instructors: 2 },
   { mod: 4, students: 16, instructors: 2 }
-];
+]
 
 module.exports = {
   mods
-};
+}
--- prototypes/problem-sets/mods.js
+++ prototypes/problem-sets/mods.js
@@ -1,4 +1,4 @@
-const { mods } = require('../datasets/mods');
+const { mods } = require('../datasets/mods')
 
 // To run the code you've written in this file, use node prototypes/problem-sets/mods.js
 
@@ -9,14 +9,14 @@ console.log('Running mods.js')
 /*
 Level 1
 
-Code: 
-  Write a function called "getStats" that returns an array of objects where the keys are mod (the number of the module) and studentsPerInstructor (how many students per instructor there are for that mod) 
+Code:
+  Write a function called "getStats" that returns an array of objects where the keys are mod (the number of the module) and studentsPerInstructor (how many students per instructor there are for that mod)
 
-Invoke: 
+Invoke:
   To print the value your function returns and confirm it is correct, invoke your function within a console.log().
 e.g.
   console.log(getStats())
-    should print --> 
+    should print -->
       [
         { mod: 1, studentsPerInstructor: 9 },
         { mod: 2, studentsPerInstructor: 11 },
@@ -25,7 +25,7 @@ e.g.
       ]
 
 Annotate:
-  After you find a solution, write out the steps of that solution.  Break them down as much as possible. 
+  After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
 
@@ -40,12 +40,10 @@ Test:
   *Refactor as needed until the test is passing
 
 Annotation:
-  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions. 
-  Take notes on the error messages that led you to fixing those details. 
+  If your tests did not immediately pass, take notes on what details you missed while building and checking your solutions.
+  Take notes on the error messages that led you to fixing those details.
 */
-
-
 
 // module.exports = {
 //   getStats
-// };
+// }
