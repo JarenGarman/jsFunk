e634e490cd86420026cc02e9574bd6304b093f3b
Jaren Garman <jarengarman@protonmail.com>
2025-02-12 17:19:34 -0500

    docs: Annotate classrooms level 4

--- prototypes/problem-sets/classrooms.js
+++ prototypes/problem-sets/classrooms.js
@@ -158,6 +158,9 @@ e.g.
 
 Annotate:
   After you find a solution, write out the steps of that solution.  Break them down as much as possible.
+
+  1. Setup a param of program
+  2. Replace 'FE' with program
 */
 
 console.log(getClassrooms("FE"))
