9dee104422d42b6606390cde5740ca084a0c0169
Jaren Garman <jarengarman@protonmail.com>
2025-02-12 20:20:07 -0500

    feat: Complete books level 1

--- prototypes/problem-sets/books.js
+++ prototypes/problem-sets/books.js
@@ -25,7 +25,13 @@ Annotate:
   After you find a solution, write out the steps of that solution.  Break them down as much as possible.
 */
 
+const removeViolence = (books) => {
+  return books.filter((book) => {
+    return book.genre !== 'Horror' && book.genre !== 'True Crime'
+  })
+}
 
+console.log(removeViolence(books))
 
 /*
 Level 2
